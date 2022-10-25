61d7deba1d68e83acaab2d94728088d8eacb682e
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-10-24 22:32:48 -0300

    criados controller para user e post

--- src/index.js
+++ src/index.js
@@ -2,24 +2,11 @@ import * as express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('GET /')
-})
+import userController from './controller/userController'
+import postController from './controller/postController'
 
-app.post('/', (req, res) => {
-  res.status(201).send('POST /')
-})
+app.use('/user', userController)
+app.use('/post', postController)
 
-app.put('/', (req, res) => {
-  res.status(400).send('PUT /')
-})
-
-app.delete('/', (req, res) => {
-  res.send('DELETE /')
-})
-
-app.patch('/', (req, res) => {
-  res.send('PATCH /')
-})
 
 app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
