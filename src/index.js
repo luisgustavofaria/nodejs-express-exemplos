e21ef1cb9ac0122b1bea987ef99d82e037d11a2a
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-10-24 21:48:54 -0300

    configurando webpack

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World 1 2 3 4')
 })
 
 app.get('/teste', function (req, res) {
